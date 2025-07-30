c9d588a2abc70b623424a0f41a076711e6967ec1
aaran <voltempl1.6@gmail.com>
2025-07-30 14:52:22 +0200

    Update game.py

--- game.py
+++ game.py
@@ -3,6 +3,6 @@ from Illusion.game_manager import IllusionBuiltInsPreset
 
 class Game(IllusionBuiltInsPreset):
     def __init__(self,hws):
-        super().__init__(False)
+        super().__init__(hws)
 
 
