799fa2a0f39e9d93e3d69de739a5a75ca89cb90a
aaran <voltempl1.6@gmail.com>
2025-10-07 11:49:25 +0200

    making to_format_string static method of c_helper

--- Illusion/helper.py
+++ Illusion/helper.py
@@ -64,7 +64,8 @@ class Helper:
         def get_time_left(self):
             return max(0, self._full_time - self._countdown)
 
-def to_format_string(color: tuple[int,int,int],size: int, font_name: str, text: str):
-    return f"{{s:{size};c:{color};f:{font_name};}}{text}"
+    @staticmethod
+    def to_format_string(color: tuple[int,int,int],size: int, font_name: str, text: str):
+        return f"{{s:{size};c:{color};f:{font_name};}}{text}"
 
 c_helper = Helper()
